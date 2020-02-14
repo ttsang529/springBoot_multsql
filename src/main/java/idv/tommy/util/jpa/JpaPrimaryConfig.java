609275d79a722c4531f2bdef1a29cad13a2bd82f
Tommy <ttsang529@gmail.com>
2020-02-14 09:30:02 +0800

    modified the config

--- src/main/java/idv/tommy/util/jpa/JpaPrimaryConfig.java
+++ src/main/java/idv/tommy/util/jpa/JpaPrimaryConfig.java
@@ -8,7 +8,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
@@ -26,10 +25,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 )
 
 public class JpaPrimaryConfig {
-    @Autowired
-    private Environment env; 
-    
-	@Autowired 
+    @Autowired 
     @Qualifier("primaryDataSource")
     private DataSource primaryDataSource;
 	
--- src/main/java/idv/tommy/util/jpa/JpaSecondaryConfig.java
+++ src/main/java/idv/tommy/util/jpa/JpaSecondaryConfig.java
@@ -8,9 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
@@ -25,9 +23,6 @@ import org.springframework.transaction.PlatformTransactionManager;
     transactionManagerRef = "secondaryTransactionManager"
 )
 public class JpaSecondaryConfig {
-    @Autowired
-    private Environment env; 
-    
     @Autowired 
     @Qualifier("secondaryDataSource")
     private DataSource secondaryDataSource;
